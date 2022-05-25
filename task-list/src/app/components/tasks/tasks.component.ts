18b1faac558b8ae6a48e307a4b5c057495ced75e
Florencia <floo.g1096@gmail.com>
2022-05-25 18:14:26 -0300

    Servicio y servidor terminados

--- task-list/src/app/components/tasks/tasks.component.ts
+++ task-list/src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock-task';
-import { Task } from 'src/app/Task';
-
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task'
 
 @Component({
   selector: 'app-tasks',
@@ -9,11 +8,16 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  Tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor(
+    private taskService: TaskService
+  ) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks)=>[
+      this.tasks = tasks
+    ]);
   }
 
 }
