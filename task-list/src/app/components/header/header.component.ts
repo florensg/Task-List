1130efaed4a0df4dd246c71cf30fab8ed07d325f
Florencia <floo.g1096@gmail.com>
2022-05-26 09:48:56 -0300

    se agrego el footer, about y demas

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
@@ -8,13 +12,26 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = 'Lista de tareas'
+  showAddTask: boolean = false;
+  subscription?: Subscription;
 
-  constructor() { }
+
+  constructor(
+    private uiService:UiService,
+    private router:Router
+  ) { 
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
+    this.uiService.toggleAddTask();
+  }
+
+  hasRouter(router: string){
+    return this.router.url === router
   }
 
 }
--- task-list/src/app/components/tasks-item/tasks-item.component.ts
+++ task-list/src/app/components/tasks-item/tasks-item.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../../Task';
+import { TASKS } from '../../mock-task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -10,10 +10,22 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TasksItemComponent implements OnInit {
   @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+  
   faTimes = faTimes;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onDelete(task: Task){
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task){
+    this.onToggleReminder.emit(task);
   }
 
 }
